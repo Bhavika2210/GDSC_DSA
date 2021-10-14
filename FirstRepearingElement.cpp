6991640539e5eb82cd2dc8e789aeb7a3d4457900
Bhavika2210 <81467761+Bhavika2210@users.noreply.github.com>
2021-10-14 09:34:10 +0530

    Create FirstRepearingElement.cpp

---
+++ FirstRepearingElement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n) {
+        // code here
+        unordered_map <int,int>MP;
+  
+  for(int i=0;i<n;i++)
+  {
+      MP[arr[i]]++;
+  }
+  for(int i=0;i<n;i++)
+  {
+      if(MP[arr[i]]>1)
+      return i+1;
+  }
+  return -1;
+    }
+};
