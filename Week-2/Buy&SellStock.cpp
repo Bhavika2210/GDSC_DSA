a76cfc0a6726284012fe8e33fb2a33f0d61706de
Bhavika2210 <81467761+Bhavika2210@users.noreply.github.com>
2021-11-16 01:24:17 +0530

    Create Buy&SellStock.cpp

---
+++ Week-2/Buy&SellStock.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=0;
+        int profit=0;
+        int n=prices.size();     
+        for(int i=0;i<prices.size() ;i++)
+        {
+            if(prices[mini]>prices[i])
+            {
+                mini=i;
+            }
+            else
+                profit=max(profit,prices[i]-prices[mini]);                           
+        }
+        return profit;
+    }
+        
+};
