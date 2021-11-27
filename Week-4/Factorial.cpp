8f46b67e54123ff2ac40de38a0e24ce51242caec
Bhavika2210 <81467761+Bhavika2210@users.noreply.github.com>
2021-11-27 23:13:30 +0530

    Create Factorial.cpp

---
+++ Week-4/Factorial.cpp
@@ -0,0 +1,7 @@
+long long int factorial(int N){
+       //code here
+       if(N<=1)
+       return 1;
+       return N*factorial(N-1);
+}
+// https://practice.geeksforgeeks.org/problems/factorial5739/1#
